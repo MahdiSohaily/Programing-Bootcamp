81ac526c56379834f7895c2276520a2dfe336e6c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-14 02:23:30 +0430

    Update style file structure base on Styleint

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -1,25 +1,25 @@
-const openMenu = document.querySelector(".menu");
-const closeMenu = document.querySelector(".close");
-const mainNav = document.querySelector(".custom-navbar");
+const openMenu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close');
+const mainNav = document.querySelector('.custom-navbar');
 
-openMenu.addEventListener("click", () => {
-  mainNav.style.display = "block";
-  mainNav.classList.toggle("open");
+openMenu.addEventListener('click', () => {
+  mainNav.style.display = 'block';
+  mainNav.classList.toggle('open');
 });
 
-closeMenu.addEventListener("click", () => {
-  mainNav.style.display = "none";
-  mainNav.classList.remove("open");
+closeMenu.addEventListener('click', () => {
+  mainNav.style.display = 'none';
+  mainNav.classList.remove('open');
 });
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   if (window.innerWidth >= 768) {
-    mainNav.classList.remove("open");
-    mainNav.style.display = "block";
+    mainNav.classList.remove('open');
+    mainNav.style.display = 'block';
   }
 });
 
-mainNav.addEventListener("click", () => {
-  mainNav.style.display = "none";
-  mainNav.classList.remove("open");
+mainNav.addEventListener('click', () => {
+  mainNav.style.display = 'none';
+  mainNav.classList.remove('open');
 });
