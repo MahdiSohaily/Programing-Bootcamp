e8d78f8095d4986488a4fe7ec905856e80b60dfe
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-13 09:41:12 +0430

    Add main navbar styles

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -8,7 +8,7 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 // Option 2
