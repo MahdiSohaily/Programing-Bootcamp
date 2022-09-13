d5df1066f7301e289a791f005f5c868e638038cf
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-14 02:19:37 +0430

    Add script to open and close mobile menu

---
+++ assets/js/starter.js
@@ -0,0 +1,24 @@
+const openMenu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".close");
+const mainNav = document.querySelector(".custom-navbar");
+
+openMenu.addEventListener("click", () => {
+  mainNav.style.display = "block";
+  mainNav.classList.toggle("open");
+});
+
+closeMenu.addEventListener("click", () => {
+  mainNav.style.display = "none";
+  mainNav.classList.remove("open");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    mainNav.classList.remove("open");
+  }
+});
+
+mainNav.addEventListener("click", () => {
+  mainNav.style.display = "none";
+  mainNav.classList.remove("open");
+});
