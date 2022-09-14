433a9a09b4627baca2e0cfdbbd40aab320ff0a70
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-14 17:59:00 +0430

    Add preloader behavior to website

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -34,6 +34,8 @@ let i = 0;
 window.addEventListener('load', typeWriter);
 
 function typeWriter() {
+  const preloader = document.getElementById('preloader');
+  preloader.style.display = 'none';
   if (i < title.length) {
     header.innerHTML += title.charAt(i);
     i++;
@@ -42,33 +44,32 @@ function typeWriter() {
 }
 
 // generating dynamic content for speakers section
-
+const container = document.querySelector('.speaker-list');
 fetch('./data.json')
   .then((response) => response.json())
-  .then((json) => createGraph(json));
-
-  function createElem(elem) {
-    const template = `<article class="speaker">
-        <div class="speaker-image">
-          <img src=" ${elem.picture}" width="120" height="150" alt="${elem.fullName}">
-        </div>
-        <div class="details">
-          <div class="information">
-            <p class="text-secondary">${elem.fullName}</p>
-            <p class="position text-primary small">${elem.position}</p>
-            <div class=" divider ml-0 w-10 border-bottom border-secondary-300 border-200"></div>
-          </div>
-          <blockquote>
-          ${elem.quote}
-          </blockquote>
-        </div>
-    </article>`;
-    return template;
-  };
-  const container = document.querySelector('.speaker-list');
-function createGraph(json) {
+  .then((json) => createElem(json));
+function createElem(json) {
   const obj = json['speakers'];
   for (let counter = 0; counter < obj.length; counter++) {
-    container.innerHTML += createElem(obj[counter]);
+    container.innerHTML += appendElem(obj[counter]);
   }
+}
+
+function appendElem(elem) {
+  const template = `<article class="speaker">
+      <div class="speaker-image">
+        <img src=" ${elem.picture}" width="120" height="150" alt="${elem.fullName}">
+      </div>
+      <div class="details">
+        <div class="information">
+          <p class="text-secondary">${elem.fullName}</p>
+          <p class="position text-primary small">${elem.position}</p>
+          <div class=" divider ml-0 w-10 border-bottom border-secondary-300 border-200"></div>
+        </div>
+        <blockquote>
+        ${elem.quote}
+        </blockquote>
+      </div>
+  </article>`;
+  return template;
 }
