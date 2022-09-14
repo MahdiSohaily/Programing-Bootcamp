6ca3d0fe3bc913dfe0d3c31fdc533b838ded7658
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-14 11:36:38 +0430

    Add script to read data from json file and create dynamic data

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -26,18 +26,49 @@ mainNav.addEventListener('click', () => {
   mainNav.classList.remove('open');
 });
 
-
 // leading-header animation;
 const header = document.getElementById('leading-header');
 const title = 'A stream of endless knowledge.';
 const speed = 50;
 let i = 0;
-window.addEventListener('load', typeWriter)
+window.addEventListener('load', typeWriter);
 
 function typeWriter() {
   if (i < title.length) {
     header.innerHTML += title.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+  }
+}
+
+// generating dynamic content for speakers section
+
+fetch('./data.json')
+  .then((response) => response.json())
+  .then((json) => createGraph(json));
+
+  function createElem(elem) {
+    const template = `<article class="speaker">
+        <div class="speaker-image">
+          <img src=" ${elem.picture}" width="120" height="150" alt="${elem.fullName}">
+        </div>
+        <div class="details">
+          <div class="information">
+            <p class="text-secondary">${elem.fullName}</p>
+            <p class="position text-primary small">${elem.position}</p>
+            <div class=" divider ml-0 w-10 border-bottom border-secondary-300 border-200"></div>
+          </div>
+          <blockquote>
+          ${elem.quote}
+          </blockquote>
+        </div>
+    </article>`;
+    return template;
+  };
+  const container = document.querySelector('.speaker-list');
+function createGraph(json) {
+  const obj = json['speakers'];
+  for (let counter = 0; counter < obj.length; counter++) {
+    container.innerHTML += createElem(obj[counter]);
   }
 }
